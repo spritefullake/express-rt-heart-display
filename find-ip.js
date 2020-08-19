de0b92f2e58f8e3961ff01de79cb47a59e6a7c36
spritefullake <42ahmad@gmail.com>
2020-08-19 16:42:51 -0400

    checking my assumptions

--- find-ip.js
+++ find-ip.js
@@ -1,6 +1,8 @@
 var os = require( 'os' );
 var networkInterfaces = os.networkInterfaces( );
+console.log("All network interfaces: ",networkInterfaces);
 const homeInterfaces = Object.values(networkInterfaces).flat();
-const ipAddress = homeInterfaces.filter(({family, internal}) => family === 'IPv4' && !internal)[0].address
+const preferredIp = null; //or replace with your actual local IP
+const ipAddress = preferredIp || homeInterfaces.filter(({family, internal}) => family === 'IPv4' && !internal)[0].address
 console.log(ipAddress);
 module.exports = {ipAddress};
