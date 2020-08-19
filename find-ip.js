0606e6e9f3608491392cc640b8b95cba66868196
spritefullake <42ahmad@gmail.com>
2020-08-19 16:40:20 -0400

    fixed ip problems maybe

--- find-ip.js
+++ find-ip.js
@@ -1,6 +1,6 @@
 var os = require( 'os' );
 var networkInterfaces = os.networkInterfaces( );
 const homeInterfaces = Object.values(networkInterfaces).flat();
-const ipAddress = homeInterfaces.filter(({family}) => family === 'IPv4')[0].address
+const ipAddress = homeInterfaces.filter(({family, internal}) => family === 'IPv4' && !internal)[0].address
 console.log(ipAddress);
 module.exports = {ipAddress};
